b0efce1f938ccd8c5edb7037feb127e82637fbce
Rytas Stalgys <Rytas.Stalgys@gmail.com>
2024-03-19 16:20:48 +0200

    implemented split_into_two method

--- war_card_game.py
+++ war_card_game.py
@@ -4,6 +4,7 @@ import random
 class Deck:
     def __init__(self):
         self.deck = []
+        self.shuffled_deck = []
 
     def create_deck(self) -> List[str]:
         values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
@@ -12,24 +13,51 @@ class Deck:
             for symbol in symbols:
                 self.deck.append(value + symbol)
     
-    def remove_card(self) -> List[str]:
-        self.deck.pop(0)
+    # def remove_card(self) -> List[str]:
+    #     self.deck.pop(0)
 
     def shuffle_deck(self) -> List[str]:
-        random.shuffle(self.deck)
+        # shuffled_deck = random.shuffle(self.deck)
+        # return shuffled_deck
+        self.shuffled_deck = self.deck[:]  # Make a copy of the original deck
+        random.shuffle(self.shuffled_deck)
+        return self.shuffled_deck
+
     
     def get_deck(self) -> List[str]:
         return self.deck
 
+class Player:
+    def __init__(self, shuffled_deck):
+        self.shuffled_deck = shuffled_deck
+        self.player_deck = []
+        self.opponent_deck = []
+
+    def split_into_two(self):
+        # print(self.shuffled_deck)
+        midpoint = len(self.shuffled_deck) // 2
+        player_deck = self.shuffled_deck[:midpoint]
+        opponent_deck = self.shuffled_deck[midpoint:]
+        self.player_deck = player_deck
+        self.opponent_deck = opponent_deck
+
+        # return player_deck, opponent_deck
+
+class Table:
+    ...
 
 
 def main():
     deck = Deck()
     deck.create_deck()
-    deck.remove_card()
-    deck.shuffle_deck()
 
-    print(deck.get_deck())
+    shuffled_deck = deck.shuffle_deck()
+    # print("Shuffled Deck:", shuffled_deck)
+
+    player = Player(shuffled_deck)
+    player.split_into_two()
+    print(player.opponent_deck, player.player_deck)
+
 
 
 if __name__ == "__main__":
